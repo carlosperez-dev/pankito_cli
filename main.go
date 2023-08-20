9d00a217c10a5379da43eb3a481c3aceb09ad9e0
Carlos Perez <cjperezvelez@gmail.com>
2023-08-20 15:32:03 +0100

    Add deck concept, make refactors to support it and edit UI

--- main.go
+++ main.go
@@ -17,9 +17,11 @@ import (
 )
 
 func main() {
+
 	menu := []string{
 		"Add Card",
 		"Review Deck",
+		"Create Deck",
 		"Quit",
 	}
 	file := "pankito"
@@ -34,13 +36,18 @@ func main() {
 func OpenMenu(menu []string, db *DB) {
 	option := SelectOption(menu)
 	if option == menu[0] {
-		card := CreateCard()
+		card := CreateCard(db)
 		AddNewCard(db, card)
 		OpenMenu(menu, db)
 	} else if option == menu[1] {
-		StartDeckReview(GetCardsToReview(db), db)
+		deckId := GetDeckOfCard(db)
+		StartDeckReview(GetCardsToReview(db, deckId), db)
 		OpenMenu(menu, db)
 	} else if option == menu[2] {
+		deck := CreateDeck()
+		AddNewDeck(db, deck)
+		OpenMenu(menu, db)
+	} else if option == menu[3] {
 		os.Exit(0)
 	}
 }
@@ -51,6 +58,7 @@ type DB struct {
 
 type BaseCard struct {
 	Id         int
+	DeckId     int
 	Front      string
 	Back       string
 	Interval   int
@@ -59,12 +67,17 @@ type BaseCard struct {
 	ReviewDate time.Time
 }
 
+type BaseDeck struct {
+	Id   int
+	Name string
+}
+
 func newDb(file string) (*DB, error) {
 	file = strings.TrimSpace(file)
 	if file == "" {
 		return nil, errors.New("cannot instantiate a db with empty/whitespace name")
 	}
-	create := "CREATE TABLE IF NOT EXISTS [Cards] ( Id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, Front TEXT NOT NULL, Back TEXT NOT NULL, Interval INTEGER NOT NULL, EaseFactor DECIMAL(10,8) NOT NULL, Repetition INTEGER NOT NULL, ReviewDate DATETIME NOT NULL);"
+	create := "CREATE TABLE IF NOT EXISTS [Cards] ( Id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, DeckId INTEGER NOT NULL,Front TEXT NOT NULL, Back TEXT NOT NULL, Interval INTEGER NOT NULL, EaseFactor DECIMAL(10,8) NOT NULL, Repetition INTEGER NOT NULL, ReviewDate DATETIME NOT NULL, FOREIGN KEY(DeckId) REFERENCES Decks(Id)); CREATE TABLE IF NOT EXISTS [Decks] ( Id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, Name TEXT NOT NULL);"
 
 	db, err := sql.Open("sqlite3", file+".db")
 	if err != nil {
@@ -79,9 +92,17 @@ func newDb(file string) (*DB, error) {
 	}, nil
 }
 
+func AddNewDeck(db *DB, deck BaseDeck) {
+	stmt := "INSERT INTO Decks(Name) VALUES (?)"
+	if _, err := db.db.Exec(stmt, deck.Name); err != nil {
+		log.Fatal("Failed to execute INSERT", err)
+	}
+	log.Printf("Deck added")
+}
+
 func AddNewCard(db *DB, card BaseCard) {
-	stmt := "INSERT INTO Cards(Front, Back, Interval, EaseFactor, Repetition, ReviewDate) VALUES (?, ?, ?, ?, ?, ?)"
-	if _, err := db.db.Exec(stmt, card.Front, card.Back, 0, 2.5, 0, time.Now()); err != nil {
+	stmt := "INSERT INTO Cards(DeckId, Front, Back, Interval, EaseFactor, Repetition, ReviewDate) VALUES (?, ?, ?, ?, ?, ?, ?)"
+	if _, err := db.db.Exec(stmt, card.DeckId, card.Front, card.Back, 0, 2.5, 0, time.Now()); err != nil {
 		log.Fatal("Failed to execute INSERT", err)
 	}
 	log.Printf("Card added")
@@ -103,12 +124,14 @@ func AddCardsToReview(db *DB) []BaseCard {
 	return deck
 }
 
-func CreateCard() BaseCard {
+func CreateCard(db *DB) BaseCard {
+	deckId := GetDeckOfCard(db)
 	front := GetFrontOfCard()
 	back := GetBackOfCard()
 
 	return BaseCard{
 		Id:         0,
+		DeckId:     deckId,
 		Front:      front,
 		Back:       back,
 		Interval:   0,
@@ -117,6 +140,66 @@ func CreateCard() BaseCard {
 		ReviewDate: time.Time{},
 	}
 
+}
+
+func GetDeckOfCard(db *DB) int {
+	decks := GetExistingDecks(db)
+	templates := &promptui.SelectTemplates{
+		Active:   "➡️ {{ .Name | blue | underline}}",
+		Inactive: " {{ .Name | faint }}",
+		Selected: "✅ {{ .Name | green }}",
+	}
+
+	searcher := func(input string, index int) bool {
+		deck := decks[index]
+		name := strings.Replace(strings.ToLower(deck.Name), " ", "", -1)
+		input = strings.Replace(strings.ToLower(input), " ", "", -1)
+
+		return strings.Contains(name, input)
+	}
+	prompt := promptui.Select{
+		Label:     "Select deck",
+		Items:     decks,
+		Templates: templates,
+		Searcher:  searcher,
+	}
+
+	i, _, err := prompt.Run()
+
+	if err != nil {
+		log.Fatalf("Prompt failed %v\n", err)
+	}
+	return decks[i].Id
+
+}
+
+func GetExistingDecks(db *DB) []BaseDeck {
+	stmt := "SELECT Id, Name FROM Decks ORDER BY Id"
+	rows, err := db.db.Query(stmt)
+	if err != nil {
+		log.Fatal("Error querying for cards", err)
+	}
+
+	defer rows.Close()
+
+	data := []BaseDeck{}
+	for rows.Next() {
+		i := BaseDeck{}
+		err = rows.Scan(&i.Id, &i.Name)
+		if err != nil {
+			log.Printf("Error occurred whilst mapping decks Id: %v - error: %v", &i.Id, err)
+		}
+		data = append(data, i)
+	}
+	return data
+}
+
+func CreateDeck() BaseDeck {
+	name := GetNameOfDeck()
+	return BaseDeck{
+		Id:   0,
+		Name: name,
+	}
 }
 
 func GetBackOfCard() string {
@@ -167,9 +250,33 @@ func GetFrontOfCard() string {
 	return result
 }
 
-func GetCardsToReview(db *DB) []BaseCard {
-	stmt := "SELECT * FROM Cards WHERE datetime(ReviewDate) <= datetime('now') ORDER BY ReviewDate"
-	rows, err := db.db.Query(stmt)
+func GetNameOfDeck() string {
+	validate := func(input string) error {
+		if len(input) == 0 {
+			return errors.New("name of deck cannot be empty")
+		}
+		return nil
+	}
+
+	name := ""
+
+	prompt := promptui.Prompt{
+		Label:    "Deck name",
+		Validate: validate,
+		Default:  name,
+	}
+
+	result, err := prompt.Run()
+
+	if err != nil {
+		fmt.Printf("Prompt failed %v\n", err)
+	}
+	return result
+}
+
+func GetCardsToReview(db *DB, deckId int) []BaseCard {
+	stmt := "SELECT * FROM Cards WHERE datetime(ReviewDate) <= datetime('now') AND DeckId = ? ORDER BY ReviewDate"
+	rows, err := db.db.Query(stmt, deckId)
 	if err != nil {
 		log.Fatal("Error querying for cards", err)
 	}
@@ -179,14 +286,13 @@ func GetCardsToReview(db *DB) []BaseCard {
 	data := []BaseCard{}
 	for rows.Next() {
 		i := BaseCard{}
-		err = rows.Scan(&i.Id, &i.Front, &i.Back, &i.Interval, &i.EaseFactor, &i.Repetition, &i.ReviewDate)
+		err = rows.Scan(&i.Id, &i.DeckId, &i.Front, &i.Back, &i.Interval, &i.EaseFactor, &i.Repetition, &i.ReviewDate)
 		if err != nil {
 			log.Printf("Error occurred whilst mapping cards Id: %v - error: %v", &i.Id, err)
 		}
 		data = append(data, i)
 	}
 	return data
-
 }
 
 func StartDeckReview(deck []BaseCard, db *DB) []BaseCard {
@@ -228,7 +334,7 @@ func ViewFrontAndBack(card *BaseCard) string {
 func ViewFront(card *BaseCard) {
 	format := fmt.Sprintf("\x1b[%dm\n > %s \x1b[0m", 34, "Press 'Enter' to show answer")
 	fmt.Println(format)
-	format = fmt.Sprintf("\x1b[%dm\n%s\x1b[0m", 33, card.Front)
+	format = fmt.Sprintf("\x1b[%dm\n%s\x1b[0m", 93, card.Front)
 	fmt.Println(format)
 	bufio.NewReader(os.Stdin).ReadString('\n')
 }
